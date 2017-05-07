f4e7e753396134c1c9fc0e03044c89851f923f1f
SO HYUN SEOP <iodesme@gmail.com>
2017-05-07 22:21:51 +0900

    설치 뷰 모델에 열거형 및 객체 구현

--- Linpress.Bootstrapper.Shell/ViewModels/InstallViewModel.cs
+++ Linpress.Bootstrapper.Shell/ViewModels/InstallViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Linpress.Bootstrapper.Shell.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,34 @@ using System.Threading.Tasks;
 
 namespace Linpress.Bootstrapper.Shell.ViewModels
 {
-    class InstallViewModel
+    public class InstallViewModel : BaseModel
     {
+        #region 객체
+        private InstallState installState;
+        public UpdateState updateState;
+        #endregion
+
+        #region 열거형
+        public enum InstallState
+        {
+            Initializing,
+            Present,
+            NotPresent,
+            Applying,
+            Cancelled,
+            Applied,
+            Failed,
+        }
+
+        public enum UpdateState
+        {
+            Unknown,
+            Initializing,
+            Checking,
+            Current,
+            Available,
+            Failed,
+        }
+        #endregion
     }
 }
